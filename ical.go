83a918b6d27c2f02da50f3dd2e7e76a0415fb7fa
Felix Sun <sunfmin@gmail.com>
2015-01-19 18:28:46 +0800

    Add output for normal event

--- ical.go
+++ ical.go
@@ -1,11 +1,11 @@
 package ical
 
 import (
-	"strings"
-	"time"	
-	"strconv"	
 	"errors"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // ~/Library/Calendars/05C38275-CBA9-4321-8457-2DC326799AE8.calendar/Events
@@ -27,11 +27,8 @@ import (
 // END:VTODO
 // END:VCALENDAR
 
-
 // STATUS: NEEDS-ACTION / COMPLETE
 // PERCENT-COMPLETE - enlever / 100
-
-
 
 // DTSTART;TZID=Asia/Harbin:20130817T000000
 // DUE;TZID=Asia/Harbin:20130817T000000
@@ -42,7 +39,6 @@ import (
 // DESCRIPTION:Avertissement événement
 // ACTION:DISPLAY
 // END:VALARM
-
 
 // TODO: TIME ZONE
 
@@ -61,24 +57,28 @@ func EscapeTextType(input string) string {
 	output := strings.Replace(input, "\\", "\\\\", -1)
 	output = strings.Replace(output, ";", "\\;", -1)
 	output = strings.Replace(output, ",", "\\,", -1)
-	output = strings.Replace(output, "\n", "\\n", -1)	
+	output = strings.Replace(output, "\n", "\\n", -1)
 	return output
 }
 
 type Node struct {
-	Name string
-	Value string
-	Type int // 1 = Object, 0 = Name/Value
+	Name       string
+	Value      string
+	Type       int // 1 = Object, 0 = Name/Value
 	Parameters map[string]string
-	Children []*Node
+	Children   []*Node
 }
 
 func (this *Node) Parameter(name string, defaultValue string) string {
-	if len(this.Parameters) <= 0 { return defaultValue }
+	if len(this.Parameters) <= 0 {
+		return defaultValue
+	}
 	v, ok := this.Parameters[name]
-	if !ok { return defaultValue }
+	if !ok {
+		return defaultValue
+	}
 	return v
-} 
+}
 
 func (this *Node) ChildrenByName(name string) []*Node {
 	var output []*Node
@@ -112,29 +112,39 @@ func (this *Node) PropDate(name string, defaultValue time.Time) time.Time {
 	// 20130816T160000Z
 	// Mon Jan 2 15:04:05 -0700 MST 2006
 	// DUE;TZID=Asia/Harbin:20130817T000000
-	
+
 	node := this.ChildByName(name)
-	if node == nil { return defaultValue }
+	if node == nil {
+		return defaultValue
+	}
 	tzid := node.Parameter("TZID", "")
 	var output time.Time
 	var err error
 	if tzid != "" {
 		loc, err := time.LoadLocation(tzid)
-		if err != nil { panic(err) }
-		output, err = time.ParseInLocation("20060102T150405", node.Value, loc)		
+		if err != nil {
+			panic(err)
+		}
+		output, err = time.ParseInLocation("20060102T150405", node.Value, loc)
 	} else {
 		output, err = time.Parse("20060102T150405Z", node.Value)
 	}
-	
-	if err != nil { panic(err) }
+
+	if err != nil {
+		panic(err)
+	}
 	return output
 }
 
 func (this *Node) PropInt(name string, defaultValue int) int {
 	n := this.PropString(name, "")
-	if n == "" { return defaultValue }
+	if n == "" {
+		return defaultValue
+	}
 	output, err := strconv.Atoi(n)
-	if err != nil { panic(err) }
+	if err != nil {
+		panic(err)
+	}
 	return output
 }
 
@@ -146,7 +156,7 @@ func (this *Node) String() string {
 	} else {
 		s += this.Name
 		s += ":" + this.Value
-		s += "\n"		
+		s += "\n"
 	}
 	for _, child := range this.Children {
 		s += child.String()
@@ -161,15 +171,15 @@ func (this *Node) String() string {
 func UnescapeTextType(s string) string {
 	s = strings.Replace(s, "\\;", ";", -1)
 	s = strings.Replace(s, "\\,", ",", -1)
-	s = strings.Replace(s, "\\n", "\n", -1)	
-	s = strings.Replace(s, "\\\\", "\\", -1)	
+	s = strings.Replace(s, "\\n", "\n", -1)
+	s = strings.Replace(s, "\\\\", "\\", -1)
 	return s
 }
 
 func ParseTextType(lines []string, lineIndex int) (string, int) {
 	line := lines[lineIndex]
 	colonIndex := strings.Index(line, ":")
-	output := strings.TrimSpace(line[colonIndex+1:len(line)])
+	output := strings.TrimSpace(line[colonIndex+1 : len(line)])
 	lineIndex++
 	for {
 		line := lines[lineIndex]
@@ -187,7 +197,7 @@ func EncodeTextType(s string) string {
 	s = EscapeTextType(s)
 	lineLength := 0
 	for _, c := range s {
-		if lineLength + len(string(c)) > 75 {
+		if lineLength+len(string(c)) > 75 {
 			output += "\n "
 			lineLength = 1
 		}
@@ -215,7 +225,9 @@ func EncodeTextType(s string) string {
 // END:VCALENDAR
 
 func TodoFromNode(node *Node) Todo {
-	if node.Name != "VTODO" { panic("Node is not a VTODO") }
+	if node.Name != "VTODO" {
+		panic("Node is not a VTODO")
+	}
 
 	var todo Todo
 	todo.SetId(node.PropString("UID", ""))
@@ -227,7 +239,7 @@ func TodoFromNode(node *Node) Todo {
 	todo.SetModifiedDate(node.PropDate("DTSTAMP", time.Time{}))
 	todo.SetPriority(node.PropInt("PRIORITY", 0))
 	todo.SetPercentComplete(node.PropInt("PERCENT-COMPLETE", 0))
-	return todo	
+	return todo
 }
 
 func ParseCalendarNode(lines []string, lineIndex int) (*Node, bool, error, int) {
@@ -245,12 +257,14 @@ func ParseCalendarNode(lines []string, lineIndex int) (*Node, bool, error, int) 
 		parameters = make(map[string]string)
 		for i := 1; i < len(splitted); i++ {
 			p := strings.Split(splitted[i], "=")
-			if len(p) != 2 { panic("Invalid parameter format: " + name) } 
+			if len(p) != 2 {
+				panic("Invalid parameter format: " + name)
+			}
 			parameters[p[0]] = p[1]
-		}	
+		}
 	}
-	value := line[colonIndex+1:len(line)]
-	
+	value := line[colonIndex+1 : len(line)]
+
 	if name == "BEGIN" {
 		node := new(Node)
 		node.Name = value
@@ -283,7 +297,7 @@ func ParseCalendarNode(lines []string, lineIndex int) (*Node, bool, error, int) 
 			return node, false, nil, lineIndex + 1
 		}
 	}
-	
+
 	panic("Unreachable")
 	return nil, false, nil, lineIndex + 1
 }
@@ -293,41 +307,74 @@ type Calendar struct {
 }
 
 type CalendarItem struct {
-	id string
-	summary string
-	description string
-	priority int // 0..9 (O -> none, 1 -> highest, 9 -> lowest)
+	id              string
+	summary         string
+	description     string
+	priority        int // 0..9 (O -> none, 1 -> highest, 9 -> lowest)
 	percentComplete int
-	createdDate time.Time
-	modifiedDate time.Time
-	completedDate time.Time
-	startDate time.Time
-	alarmDate time.Time
-	sequence int
+	createdDate     time.Time
+	modifiedDate    time.Time
+	completedDate   time.Time
+	startDate       time.Time
+	endDate         time.Time
+	alarmDate       time.Time
+	sequence        int
+	freq            string
 }
 
-func (this *CalendarItem) SetId(v string) { this.id = v }
-func (this *CalendarItem) Id() string {	return this.id }
-func (this *CalendarItem) SetSummary(v string) { this.summary = v }
-func (this *CalendarItem) Summary() string { return this.summary }
-func (this *CalendarItem) SetDescription(v string) { this.description = v }
-func (this *CalendarItem) Description() string { return this.description }
-func (this *CalendarItem) SetPriority(v int) { this.priority = v }
-func (this *CalendarItem) Priority() int { return this.priority }
-func (this *CalendarItem) SetPercentComplete(v int) { this.percentComplete = v }
-func (this *CalendarItem) PercentComplete() int { return this.percentComplete }
-func (this *CalendarItem) SetCreatedDate(v time.Time) { this.createdDate = v }
-func (this *CalendarItem) CreatedDate() time.Time { return this.createdDate }
-func (this *CalendarItem) SetModifiedDate(v time.Time) { this.modifiedDate = v }
-func (this *CalendarItem) ModifiedDate() time.Time { return this.modifiedDate }
+func (this *CalendarItem) SetId(v string)               { this.id = v }
+func (this *CalendarItem) Id() string                   { return this.id }
+func (this *CalendarItem) SetSummary(v string)          { this.summary = v }
+func (this *CalendarItem) Summary() string              { return this.summary }
+func (this *CalendarItem) SetDescription(v string)      { this.description = v }
+func (this *CalendarItem) Description() string          { return this.description }
+func (this *CalendarItem) SetPriority(v int)            { this.priority = v }
+func (this *CalendarItem) Priority() int                { return this.priority }
+func (this *CalendarItem) SetPercentComplete(v int)     { this.percentComplete = v }
+func (this *CalendarItem) PercentComplete() int         { return this.percentComplete }
+func (this *CalendarItem) SetCreatedDate(v time.Time)   { this.createdDate = v }
+func (this *CalendarItem) CreatedDate() time.Time       { return this.createdDate }
+func (this *CalendarItem) SetModifiedDate(v time.Time)  { this.modifiedDate = v }
+func (this *CalendarItem) ModifiedDate() time.Time      { return this.modifiedDate }
 func (this *CalendarItem) SetCompletedDate(v time.Time) { this.completedDate = v }
-func (this *CalendarItem) CompletedDate() time.Time { return this.completedDate }
-func (this *CalendarItem) SetStartDate(v time.Time) { this.startDate = v }
-func (this *CalendarItem) StartDate() time.Time { return this.startDate }
-func (this *CalendarItem) SetAlarmDate(v time.Time) { this.alarmDate = v }
-func (this *CalendarItem) AlarmDate() time.Time { return this.alarmDate }
-func (this *CalendarItem) SetSequence(v int) { this.sequence = v }
-func (this *CalendarItem) Sequence() int { return this.sequence }
+func (this *CalendarItem) CompletedDate() time.Time     { return this.completedDate }
+func (this *CalendarItem) SetStartDate(v time.Time)     { this.startDate = v }
+func (this *CalendarItem) StartDate() time.Time         { return this.startDate }
+func (this *CalendarItem) SetEndDate(v time.Time)       { this.endDate = v }
+func (this *CalendarItem) EndDate() time.Time           { return this.endDate }
+func (this *CalendarItem) SetAlarmDate(v time.Time)     { this.alarmDate = v }
+func (this *CalendarItem) AlarmDate() time.Time         { return this.alarmDate }
+func (this *CalendarItem) SetSequence(v int)            { this.sequence = v }
+func (this *CalendarItem) Sequence() int                { return this.sequence }
+func (this *CalendarItem) SetFreq(v string)             { this.freq = v }
+func (this *CalendarItem) Freq() string                 { return this.freq } // WEEKLY, DAILY
+
+// BEGIN:VEVENT
+// UID:549b906570677f8379000023
+// DTSTAMP:20141216T010000Z
+// DTSTART:20141216T010000Z
+// DTEND:20141230T010000Z
+// SUMMARY:这个是12月16日开始的日程
+// END:VEVENT
+
+func (this *CalendarItem) ICalString(target string) string {
+	s := "BEGIN:VEVENT\n"
+
+	s += EncodeDateProperty("CREATED", this.CreatedDate()) + "\n"
+	s += "UID:" + this.Id() + "\n"
+	s += "SUMMARY:" + EscapeTextType(this.Summary()) + "\n"
+	s += EncodeDateProperty("DTSTAMP", this.ModifiedDate()) + "\n"
+	s += EncodeDateProperty("DTSTART", this.StartDate()) + "\n"
+	s += EncodeDateProperty("DTEND", this.EndDate()) + "\n"
+	if this.Description() != "" {
+		s += "DESCRIPTION:" + EncodeTextType(this.Description()) + "\n"
+	}
+	if this.Freq() != "" {
+		s += "RRULE:FREQ=" + this.Freq() + ";COUNT=10\n"
+	}
+	s += "END:VEVENT\n"
+	return s
+}
 
 type Todo struct {
 	CalendarItem
@@ -335,11 +382,11 @@ type Todo struct {
 }
 
 func (this *Todo) SetDueDate(v time.Time) { this.dueDate = v }
-func (this *Todo) DueDate() time.Time { return this.dueDate }
+func (this *Todo) DueDate() time.Time     { return this.dueDate }
 
 func (this *Todo) ICalString(target string) string {
 	s := "BEGIN:VTODO\n"
-	
+
 	if target == "macTodo" {
 		status := "NEEDS-ACTION"
 		if this.PercentComplete() == 100 {
@@ -347,10 +394,13 @@ func (this *Todo) ICalString(target string) string {
 		}
 		s += "STATUS:" + status + "\n"
 	}
-	
+
 	s += EncodeDateProperty("CREATED", this.CreatedDate()) + "\n"
 	s += "UID:" + this.Id() + "\n"
 	s += "SUMMARY:" + EscapeTextType(this.Summary()) + "\n"
+	if !this.DueDate().IsZero() {
+		s += EncodeDateProperty("DUE", this.DueDate()) + "\n"
+	}
 	if this.PercentComplete() == 100 && !this.CompletedDate().IsZero() {
 		s += EncodeDateProperty("COMPLETED", this.CompletedDate()) + "\n"
 	}
@@ -367,8 +417,8 @@ func (this *Todo) ICalString(target string) string {
 	if this.Description() != "" {
 		s += "DESCRIPTION:" + EncodeTextType(this.Description()) + "\n"
 	}
-	
+
 	s += "END:VTODO\n"
-	
+
 	return s
 }
